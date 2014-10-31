e836612c1ed16ee999cbd31bf72e47dd73e83d67
Lenny <scot0530@morris.umn.edu>
2014-10-31 14:48:13 -0500

    I added comments to show what it prints

--- 2014FirstLesson/src/pkg2014firstlesson/Main.java
+++ 2014FirstLesson/src/pkg2014firstlesson/Main.java
@@ -22,22 +22,28 @@ public class Main {
         // Integer Stuff
         int wholeNumber = 50;
         int secondWholeNumber = 36;
-        System.out.println(wholeNumber + secondWholeNumber);
+        System.out.println(wholeNumber + secondWholeNumber); //prints 86
         
-        System.out.println("the value of wholeNumber is " + wholeNumber);
+        System.out.println("the value of wholeNumber is " + wholeNumber); // prints "the value of wholeNumber is 50"
         wholeNumber = 100;
-        System.out.println("the value of wholeNumber after the change is " + wholeNumber);
+        //prints "the value of wholeNumber after the cahnge is 100"
+        System.out.println("the value of wholeNumber after the change is " + wholeNumber); 
         
+        // prints "the value of wholeNumber after added with secondWholeNumber is 136"
         System.out.println("the value of wholeNumber after added with secondWholeNumber is " + (wholeNumber + secondWholeNumber));
+        // prints "subtraction 64"
         System.out.println("subtraction " + (wholeNumber - secondWholeNumber ));
+        // prints "Divison 2"
         System.out.println("Division " + wholeNumber / secondWholeNumber);
         
         // Double Stuff
         double decimal = 1;
         double secondDecimal = 3;
+        // prints "1 / 3: 0.3333333333333333"
         System.out.println("1 / 3: " + decimal / secondDecimal);
         
         double result = 1.0 / 3;
+        // prints "1 / 3: 0.3333333333333333"
         System.out.println("1 / 3: " + result);
     }
 }
